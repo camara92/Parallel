129f18f6a0d523f3809ea7885895acf28f0b2261
Kevin-lepersonnic <kek.lp.56@gmail.com>
2022-09-07 11:07:45 +0200

    second commit develop, ready to feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ChallengesComponent } from './challenges/challenges.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    ChallengesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
