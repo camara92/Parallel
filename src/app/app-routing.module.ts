28abb63ca53bbc4e167904f8333c5b4c5976b06e
Kevin-lepersonnic <kek.lp.56@gmail.com>
2022-09-08 14:20:07 +0200

    WIP - start chellenge-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ChallengesComponent } from './challenges/challenges.component';
 
 const routes: Routes = [
   { path: '', component: NavbarComponent },
   { path: 'dashboard', component: NavbarComponent },
-  { path: 'challenges', component: NavbarComponent },
+  { path: 'challenges', component: ChallengesComponent },
   { path: 'about', component: NavbarComponent },
 ];
 
