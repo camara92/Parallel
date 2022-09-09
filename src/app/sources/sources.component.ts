eb05701f0719f4a6b2a14c63c26f7b8257f633ac
Kevin-lepersonnic <kek.lp.56@gmail.com>
2022-09-09 16:27:11 +0200

    WIP - Create ok

--- src/app/sources/sources.component.ts
+++ src/app/sources/sources.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-sources',
@@ -8,13 +8,19 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
   styleUrls: ['./sources.component.scss']
 })
 export class SourcesComponent implements OnInit {
+  // Lors du clic sur AJOUTER UNE SOURCE, devoile le formulaire d'ajout
   showAddSource = false;
-  city:any = null;
-  constructor(private db: AngularFirestore) {
+  // Creation du formulaire d'envoi de données
+  addSourcesForm: any;
 
+  constructor(private db: AngularFirestore, private fb: FormBuilder) {
+    
   }
 
   ngOnInit(): void {
+
+    this.generateCreateForm();
+
     // let data = {
     //   title: "Super Mario",
     //   category: "Javascript"
@@ -23,5 +29,28 @@ export class SourcesComponent implements OnInit {
     // this.db.collection("sources").add(data).then(res=>{
     //   console.log(res);
     // });
+
   }
+  // function qui genere le formulaire
+  async generateCreateForm(): Promise<void> {
+      this.addSourcesForm = this.fb.group({
+        title: [''],
+        category: [''],
+      });
+  }
+
+  // function qui envoie les données du formulaire a la bdd
+  onSubmit(): void {
+    const formValue = this.addSourcesForm.value;
+    console.log(formValue);
+
+    this.db.collection("sources").add(formValue).then(res=>{
+    console.log(res);
+    });
+
+
+  }
+
+
+  
 }
